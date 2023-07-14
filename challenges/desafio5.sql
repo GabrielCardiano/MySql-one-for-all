f56ecfa01f4770fda3077f683e11d13a084882b2
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-07-14 13:56:23 -0300

    feat: req 05

---
+++ challenges/desafio5.sql
@@ -0,0 +1,8 @@
+SELECT
+    s.song_title AS cancao,
+    COUNT(p.song_id) AS reproducoes
+FROM song AS s
+    INNER JOIN play_history AS p ON p.song_id = s.song_id
+GROUP BY cancao
+ORDER BY COUNT(p.song_id) DESC, s.song_title ASC 
+LIMIT 2;
