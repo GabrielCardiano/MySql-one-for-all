5631b7246133613eb560918a0326dab9a26f25eb
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-07-15 16:34:30 -0300

    feat: req 08

---
+++ challenges/desafio8.sql
@@ -0,0 +1,7 @@
+SELECT 
+	art.name AS artista,
+    alb.album_title as album
+FROM artist AS art
+	INNER JOIN album AS alb ON alb.artist_id = art.artist_id
+	WHERE art.name = 'Elis Regina'
+    ORDER BY album;
